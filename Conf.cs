6069d6a65d70a5b100d74908b4a877e709e85376
David Srbecký <dsrbecky@gmail.com>
2010-10-16 12:14:45 +0100

    Use ampersand for escaping.  Use whitespace as delimiter.

--- Conf.cs
+++ Conf.cs
@@ -7,331 +7,305 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 
-public class ParseException: Exception
+namespace Conf
 {
-	public int Offset { get; private set; }
-	
-	public ParseException(int offset, string message): base(message)
+	/// <summary>
+	///
+	/// </summary>
+	/// <remarks>
+	///  Syntax:
+	///   (1) Arguments ::= WS? (Comment | Argument) (WS (Comment | Argument))* WS? | WS?
+	///   (2) Comment   ::= '#' [^\r\n]*
+	///   (3) WS        ::= (' ' | '\t' | '\r' | '\n')+
+	///   (4) String    ::= [^\r\n\t #'"{}=]+ | ("'" [^']* "'") | ('"' [^"]* '"')
+	///   (5) Argument  ::= [String WS? '=' WS?] String? [WS? '{' Arguments '}']
+	/// </remarks>
+	public class Parser
 	{
-		this.Offset = offset;
+		string input;
+		int curr;
+		int length;
+
+		Parser(string input)
+		{
+			this.input  = input;
+			this.curr   = 0;
+			this.length = input.Length;
+		}
+
+		public static List<Argument> Parse(string text)
+		{
+			return new Parser(text).ReadArguments(false);
+		}
+
+		public static List<Argument> ParseCommandLine()
+		{
+			Parser parser = new Parser(Environment.CommandLine);
+			string arg0;
+			parser.ReadString(out arg0); // Skip the name of the executable
+			return parser.ReadArguments(false);
+		}
+
+		List<Argument> ReadArguments(bool nested)
+		{
+			List<Argument> args = new List<Argument>();
+
+			ReadWhiteSpace();
+
+			while (curr < length) {
+				// End of nested argument list
+				if (input[curr] == '}') {
+					if (nested) {
+						break;
+					} else {
+						throw new ParseException(curr, curr, "Superfluous '}'");
+					}
+				}
+
+				// Try to parse a comment
+				if (ReadComment()) {
+					ReadWhiteSpace();
+				} else {
+					// If it is not a comment, then it must be an argument
+					Argument arg = new Argument();
+
+					// Consider this a primitive stack of the last seen but unprocessed tokens
+					string str;
+					bool haveString;
+					bool haveWhiteSpace;
+
+					// Try to push string and whitespace on our mini parsing stack
+					haveString = ReadString(out str);
+					haveWhiteSpace = haveString ? ReadWhiteSpace() : false;
+
+					// Set the name
+					if (haveString && curr < length && input[curr] == '=') {
+						// Pop the string and whitespace from the stack
+						arg.Name = str;
+						haveString = haveWhiteSpace = false;
+						curr++; // '='
+						ReadWhiteSpace();
+						// Repopulate the empty stack
+						haveString = ReadString(out str);
+						haveWhiteSpace = haveString ? ReadWhiteSpace() : false;
+					} else {
+						arg.Name = string.Empty;
+					}
+
+					bool valueSet = false;
+
+					// Set the value
+					if (haveString) {
+						// Pop string from stack
+						arg.Value = str;
+						haveString = false;
+						valueSet = true;
+					} else {
+						arg.Value = string.Empty;
+					}
+
+					// Set the value's arguments
+					if (curr < length && input[curr] == '{') {
+						Debug.Assert(!haveString);
+						haveWhiteSpace = false; // Pop whitespace from stack
+						curr++; // '{'
+						arg.ValueArguments = ReadArguments(true);
+						if (curr == length || input[curr] != '}')
+							throw new ParseException(curr, curr, "'}' Expected");
+						curr++; // '}'
+						valueSet = true;
+					} else {
+						arg.ValueArguments = null;
+					}
+
+					// At least string or argument must be present
+					if (!valueSet)
+						throw new ParseException(curr, curr, "String value or argument list expected");
+
+					// Argument was successfully read
+					Debug.Assert(!haveString);
+					args.Add(arg);
+
+					// The argument has to be followed by whitespace or end of file or '}'
+					if (!haveWhiteSpace)
+						haveWhiteSpace = ReadWhiteSpace();
+					if (!haveWhiteSpace && curr < length && input[curr] != '}') {
+						// Try to produce nice error message
+						char c = input[curr];
+						if (arg.ValueArguments == null && (c == '#' || c == '\'' || c == '"'))
+							throw new ParseException(curr, curr, "The character " + c + " is not allowed in string");
+						throw new ParseException(curr, curr, "Argument has to be followed by whitespace");
+					}
+				}
+			}
+
+			return args;
+		}
+
+		bool ReadWhiteSpace()
+		{
+			int start = curr;
+			while (curr < length && ((input[curr] <= ' ') && (input[curr] == ' ' || input[curr] == '\t' || input[curr] == '\r' || input[curr] == '\n'))) curr++;
+			return curr > start;
+		}
+
+		bool ReadComment()
+		{
+			if (curr < length && input[curr] == '#') {
+				curr++; // '#'
+				while (curr < length && input[curr] != '\r' && input[curr] != '\n') curr++;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		bool ReadString(out string str)
+		{
+			if (curr == length) {
+				str = string.Empty;
+				return false;
+			}
+
+			int start = curr;
+			char first = input[curr];
+			if (first == '"' || first == '\'') {
+				// Quoted string
+				curr++;
+				int endQuote;
+				if (curr == length || (endQuote = input.IndexOf(first, curr)) == -1)
+					throw new ParseException(start, length, "Closing quote is missing");
+				curr = endQuote + 1;
+				str = ReslveReferences(start + 1, endQuote);
+				return true;
+			} else {
+				// Unquoted string
+				while (curr < length) {
+					char c = input[curr];
+					if (('A' <= c && c <= 'z') || ('0' <= c && c <= '9') || c > 0x80) {
+						curr++; // Quick pass test for the most common characters
+					} else {
+						if (c == ' ' || c == '=' || c == '\r' || c == '\n' || c == '\t' || c == '{' || c == '}' || c == '#' || c == '\'' || c == '"')
+							break;  // String terminator
+						curr++; // Less common characters
+					}
+				}
+				str = ReslveReferences(start, curr);
+				return curr > start;
+			}
+		}
+
+		string ReslveReferences(int start, int end)
+		{
+			if (start == end)
+				return string.Empty;
+
+			if (input.IndexOf('&', start, end - start) == -1)
+				return input.Substring(start, end - start);
+
+			StringBuilder sb = new StringBuilder(end - start);
+			int pos = start;
+			while (pos < end) {
+				int amp = input.IndexOf('&', pos, end - pos);
+				if (amp == -1) {
+					sb.Append(input, pos, end - pos);
+					break;
+				} else {
+					sb.Append(input, pos, amp - pos);
+					int semicolon = input.IndexOf(';', amp, end - amp);
+					if (semicolon == -1)
+						throw new ParseException(amp, end, "';' is missing after '&'");
+					string refName = input.Substring(amp + 1, semicolon - amp - 1);
+					switch (refName) {
+						case "":     throw new ParseException(amp, amp + 2, "Empty reference");
+						case "amp":  sb.Append('&'); break;
+						case "apos": sb.Append('\''); break;
+						case "quot": sb.Append('\"'); break;
+						case "lt":   sb.Append('<'); break;
+						case "gt":   sb.Append('>'); break;
+						case "br":   sb.Append('\n'); break;
+						case "tab":  sb.Append('\t'); break;
+						case "nbsp": sb.Append((char)0xA0); break;
+						default:
+							// Unicode character
+							int utf32;
+							try {
+								if (refName.StartsWith("#x")) {
+									utf32 = int.Parse(refName.Substring(2), NumberStyles.AllowHexSpecifier);
+								} else if (refName[0] == '#') {
+									utf32 = int.Parse(refName.Substring(1), NumberStyles.None);
+								} else {
+									utf32 = int.Parse(refName, NumberStyles.AllowHexSpecifier);
+								}
+							} catch {
+								throw new ParseException(amp + 1, semicolon, "Failed to parse reference '" + refName + "'");
+							}
+							try {
+								sb.Append(char.ConvertFromUtf32(utf32));
+							} catch {
+								throw new ParseException(amp + 1, semicolon, "Invalid Unicode code point " + refName);
+							}
+							break;
+					}
+					pos = semicolon + 1;
+				}
+			}
+			return sb.ToString();
+		}
 	}
-}
 
-public abstract class Node
-{
-}
-
-public class WhiteSpace: Node
-{
-	public string RawWhiteSpace { get; set; }
-}
-
-public class Comment: Node
-{
-	public string RawComment { get; set; }
-	public string Text { get; set; }
-}
-
-public class StringValue: Node
-{
-	public string RawText { get; set; }
-	public string Text { get; set; }
-}
-
-public class ObjectValue: Node
-{
-	public string RawName { get; set; }
-	public string Name { get; set; }
-	public string RawWhiteSpace { get; set; }
-	public List<Node> Content { get; set; }
-}
-
-public class KeyValuePair: Node
-{
-	public string RawKey { get; set; }
-	public string Key { get; set; }
-	public string RawEquals { get; set; }
-	public Node Value { get; set; }
-}
-
-public class Separator: Node
-{
-	public char RawSeparator { get; set; }
-}
-
-public class Conf
-{
-	int curr;
-	string input;
-	List<Node> output;
-	
-	public Conf(string filename)
+	public class ParseException : Exception
 	{
-		curr = 0;
-		this.input = File.ReadAllText(filename);
-		this.output = ReadContent();
+		public int Start { get; private set; }
+		public int End { get; private set; }
+
+		public ParseException(int start, int end, string message)
+			: base(message)
+		{
+			this.Start = start;
+			this.End = end;
+		}
 	}
-	
-	public void DeserializeTo(Type type)
+
+	public class Argument
 	{
-		foreach (Node node in output) {
-			KeyValuePair kvp = node as KeyValuePair;
-			if (kvp != null) {
-				FieldInfo field = type.GetField(kvp.RawKey, BindingFlags.Public | BindingFlags.Static);
+		public string Name { get; set; }
+		public string Value { get; set; }
+		public List<Argument> ValueArguments { get; set; }
+	}
+
+	public static class Deserializer
+	{
+		public static void LoadAppConfig<T>()
+		{
+			// Set properties from the .conf file
+			string assembyPath = Assembly.GetEntryAssembly().Location;
+			string confPath = Path.Combine(Path.GetDirectoryName(assembyPath), Path.GetFileNameWithoutExtension(assembyPath) + ".conf");
+			if (File.Exists(confPath)) {
+				string confText = File.ReadAllText(confPath);
+				Deserialize(Parser.Parse(confText), default(T));
+			}
+
+			// Set properties from command line
+			Deserialize(Parser.ParseCommandLine(), default(T));
+		}
+
+		public static void Deserialize<T>(List<Argument> from, T to)
+		{
+			foreach (Argument arg in from) {
+				FieldInfo field = typeof(T).GetField(arg.Name, BindingFlags.Public | BindingFlags.Static);
 				if (field != null) {
-					string str = ((StringValue)kvp.Value).RawText;
-					str = UnQuote(str);
 					if (field.FieldType == typeof(TimeSpan)) {
 						TimeSpan val;
-						TimeSpan.TryParse(str, out val);
-						field.SetValue(null, val);
-					} else {
-						object val = Convert.ChangeType(str, field.FieldType);
+						TimeSpan.TryParse(arg.Value, out val);
 						field.SetValue(null, val);
 					}
-				}
-			}
-		}
-	}
-	
-	static string UnQuote(string str)
-	{
-		if (string.IsNullOrEmpty(str))
-			return str;
-		
-		char first = str[0];
-		if (first != '"' && first != '\'')
-			return str;
-		
-		StringBuilder sb = new StringBuilder(str.Length);
-		int curr = 1;
-		while (true) {
-			if (curr == str.Length) break;
-			int esc = str.IndexOf('\\', curr);
-			if (esc != -1) {
-				sb.Append(str, curr, esc - curr);
-				curr = esc + 1;
-				if (curr < str.Length) {
-					char id = str[curr];
-					curr++;
-					switch (id) {
-						case 'n':  sb.Append('\n'); break;
-						case 'r':  sb.Append('\r'); break;
-						case 't':  sb.Append('\t'); break;
-						case '"':  sb.Append('\"'); break;
-						case '\'': sb.Append('\''); break;
-						case 'u':
-							// TODO: EOF
-							string code = str.Substring(curr, 4);
-							curr += 4;
-							int codeNo = int.Parse(code, NumberStyles.HexNumber);
-							sb.Append((char)codeNo);
-							break;
-						default: sb.Append(id); break;
+					else {
+						object val = Convert.ChangeType(arg.Value, field.FieldType);
+						field.SetValue(null, val);
 					}
-				} else {
-					sb.Append('\\');
-					break;
-				}
-			} else {
-				sb.Append(str, curr, str.Length - 1 - curr);
-				if (str[str.Length - 1] != first)
-					sb.Append(str[str.Length - 1]);
-				break;
-			}
-		}
-		return sb.ToString();
-	}
-	
-	string ReadWhiteSpace()
-	{
-		int start = curr;
-		while(true) {
-			if (curr == input.Length) break;
-			char c = input[curr];
-			if (c == ' ' || c == '\t' || c == '\r' || c == '\n') {
-				curr++;
-			} else {
-				break;
-			}
-		}
-		return input.Substring(start, curr - start);
-	}
-	
-	string ReadComment()
-	{
-		int start = curr;
-		if (input[curr] == '#') {
-			curr++;
-		} else {
-			return string.Empty;
-		}
-		while(true) {
-			if (curr == input.Length) break;
-			char c = input[curr];
-			if (c == '\r') {
-				curr++;
-				if (curr < input.Length && input[curr] == '\n')
-					curr++;
-				break;
-			} else if (c == '\n') {
-				curr++;
-				break;
-			} else {
-				curr++;
-				continue;
-			}
-		}
-		return input.Substring(start, curr - start);
-	}
-	
-	void ReadStringAndWhitespace(out string str, out string ws)
-	{
-		int start = curr;
-		char first = input[curr];
-		if (first == '"' || first == '\'') {
-			// Quoted string
-			curr++;
-			while(true) {
-				if (curr == input.Length) break;
-				int endQuote = input.IndexOf(first, curr);
-				if (endQuote == -1)
-					throw new ParseException(start, "Closing quote is missing");
-				curr = endQuote + 1;
-				if (input[endQuote - 1] == '\\') {
-					// The quote was escaped
-					continue;
-				} else {
-					break;
 				}
 			}
-			str = input.Substring(start, curr - start);
-			ws = ReadWhiteSpace();
-			return;
-		} else {
-			// Unquoted string
-			while(true) {
-				if (curr == input.Length) break;
-				char c = input[curr];
-				// Quick path
-				if (('A' <= c && c <= 'z') ||
-				    ('0' <= c && c <= '9') ||
-				    ((c == ' ' || c == '\t') && curr != start))
-				{
-					curr++;
-					continue;
-				}
-				if (c == '\'' || c == '"' || c == '{' || c == '}' || c == '=' || c == ';' || c == ',' || c == '#' || c == '\r' || c == '\n' || c == ' ' || c == '\t') {
-					// Character is forbiden in unquoted string
-					break;
-				} else {
-					curr++;
-					continue;
-				}
-			}
-			// String must not end with whitesapce
-			int nonWhiteSpace = curr - 1;
-			while(nonWhiteSpace >= start && (input[nonWhiteSpace] == ' ' || input[nonWhiteSpace] == '\t')) {
-				nonWhiteSpace--;
-			}
-			int whiteSpaceStart = nonWhiteSpace + 1;
-			
-			str = input.Substring(start, whiteSpaceStart - start);
-			ws = input.Substring(whiteSpaceStart, curr - whiteSpaceStart);
-			return;
 		}
 	}
-	
-	List<Node> ReadContent()
-	{
-		List<Node> content = new List<Node>();
-		bool done = false;
-		while(!done) {
-			if (curr == input.Length) break;
-			char c = input[curr];
-			switch(c) {
-				case ' ':
-				case '\t':
-				case '\r':
-				case '\n':
-					content.Add(new WhiteSpace() { RawWhiteSpace = ReadWhiteSpace() });
-					break;
-				case '{':
-					content.Add(new ObjectValue() { Content = ReadCurlyBrackets() });
-					break;
-				case '}':
-					done = true;
-					break;
-				case ';':
-				case ',':
-					content.Add(new Separator() { RawSeparator = c });
-					curr++;
-					break;
-				case '=':
-					throw new ParseException(curr, "Equals must be preceded by key");
-				case '#':
-					content.Add(new Comment() { RawComment = ReadComment() });
-					break;
-				default:
-					string str;
-					string ws1;
-					ReadStringAndWhitespace(out str, out ws1);
-					if (curr < input.Length) {
-						c = input[curr];
-					} else {
-						c = '\0';
-					}
-					if (c == '=') {
-						curr++;
-						string ws2 = ReadWhiteSpace();
-						KeyValuePair kvp = new KeyValuePair() { RawKey = str, RawEquals = ws1 + "=" + ws2 };
-						// TODO: Is proper string?
-						string val;
-						string ws3;
-						ReadStringAndWhitespace(out val, out ws3);
-						if (curr < input.Length && input[curr] == '{') {
-							kvp.Value = new ObjectValue() { RawName = val, RawWhiteSpace = ws3, Content = ReadCurlyBrackets() };
-							content.Add(kvp);
-						} else {
-							kvp.Value = new StringValue() { RawText = val };
-							if (ws3 != string.Empty) {
-								content.Add(new WhiteSpace() { RawWhiteSpace = ws3 });
-							}
-							content.Add(kvp);
-						}
-					} else if (c == '{') {
-						content.Add(new ObjectValue() { RawName = str, RawWhiteSpace = ws1, Content = ReadCurlyBrackets() } );
-					} else {
-						content.Add(new StringValue() { RawText = str } );
-						if (ws1 != string.Empty) {
-							content.Add(new WhiteSpace() { RawWhiteSpace = ws1 } );
-						}
-					}
-					break;
-			}
-		}
-		return content;
-	}
-	
-	List<Node> ReadCurlyBrackets()
-	{
-		Debug.Assert(input[curr] == '{');
-		curr++;
-		List<Node> content = ReadContent();
-		if (curr < input.Length && input[curr] == '}') {
-			curr++;
-		} else {
-			throw new ParseException(curr, "Expected '}'");
-		}
-		return content;
-	}
-	
-	// (1) Content ::= (WhiteSpace | Comment | String | Object | KeyValuePair | ';'| ',')*
-	// (2) WhiteSpace ::= (' ' | '\t' | '\r' | '\n')+
-	// (3) Comment ::= '#' .* ('\r\n' | '\r' | '\n')
-	// (4) String ::= Char [(Char | ' ' | '\t')* Char] | QuotedString
-	// (5) Object ::= [String [WhiteSpace]] '{' Content '}'
-	// (6) KeyValuePair ::= String [WhiteSpace] '=' [WhiteSpace] (String | Object)
-	// (7) Char ::= . - ("'" | '"' | '{' | '}' | '=' | ';' | ',' | '#' | '\r' | '\n' | ' ' | '\t')
-	// (8) QuotedString ::= "'" ("\\'" | "''" | . - "'")* "'" | '"' ('\\"' | '""' | . - '"')* '"'
 }
